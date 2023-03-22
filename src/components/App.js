402f6c63944d1bfa476610dbd308b22f4db36bc7
Pranshi Sahu <115640741+pranshisahu@users.noreply.github.com>
2023-03-22 14:02:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,3 @@
-/*import { Co2Sharp } from '@mui/icons-material';
-import React from 'react'
-import '../styles/App.css';
-import ActiveAlarm from './activeAlarm';
-import AlarmContainer from './alarmContainer';
-import AlarmForm from './alarmForm';
-const App = () => {
-
-  const defaultAlarms = [
-    {
-        label: "Interview at abc", 
-        alarmTime: "08:30"
-    },
-    {
-        label: "Interview at xyz", 
-        alarmTime: "19:30"
-    },
-    {
-        label: "Interview at pqr", 
-        alarmTime: "20:45"
-    },
-  ]
-
-  return (
-    <div id="main">
-    </div>
-  )
-}
-
-
-export default App;
-*/
-
 import React from 'react'
 import '../styles/App.css';
 import AlarmContainer from './alarmContainer';
@@ -120,3 +87,4 @@ const App = () => {
 
 
 export default App;
+
